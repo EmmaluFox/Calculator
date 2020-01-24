5b22a64c89cc0d7c9bb20407318405f43a4978ed
EmmaluFox <emmalu.fox@outlook.com>
2020-01-24 17:34:45 +0000

    Added operator choice

--- Program.cs
+++ Program.cs
@@ -6,16 +6,33 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the calculator!");
-            Console.WriteLine("Enter a number:");
+            Console.WriteLine(@"Welcome to the calculator!
+************************************");
+            Console.WriteLine("Please enter the operator:");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter the first number:");
             string x = Console.ReadLine();
             int myInt = int.Parse(x);
-            
-            Console.WriteLine("Enter another number:");
+            Console.WriteLine("Enter the second number:");
             string y = Console.ReadLine();
             int myIntTwo = int.Parse(y);
-            
-            Console.Write(myInt +" multiplied by " +myIntTwo +" = " +(myInt*myIntTwo));
+            string message = $@"{myInt} {op} {myIntTwo} = ";
+            if (op == "+")
+            {
+                Console.Write($@"{message}");
+                Console.Write(myInt + myIntTwo);
+            } else if (op == "-")
+            {
+                Console.Write($@"{message}");
+                Console.Write(myInt - myIntTwo);
+            } else if (op == "/") {
+                Console.Write($@"{message}");
+                Console.Write(myInt / myIntTwo);
+            } else if (op == "*") {
+                Console.Write($@"{message}");
+                Console.Write(myInt * myIntTwo);
+            }
         }
     }
 }
