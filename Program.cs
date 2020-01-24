00200bb9ae4d12d875b83041c9c1c6dd0685c34a
EmmaluFox <emmalu.fox@outlook.com>
2020-01-24 16:12:51 +0000

    Added a simple multiplier

--- Program.cs
+++ Program.cs
@@ -7,15 +7,6 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
-/*
-            int x;
-            int y;
-            x = 7;
-            y = x + 3;
-            Console.WriteLine(y);
-            Console.ReadLine();
-*/
-
             Console.WriteLine("Enter a number:");
             string x = Console.ReadLine();
             int myInt = int.Parse(x);
@@ -25,7 +16,6 @@ namespace Calculator
             int myIntTwo = int.Parse(y);
             
             Console.Write(myInt +" multiplied by " +myIntTwo +" = " +(myInt*myIntTwo));
-
         }
     }
 }
