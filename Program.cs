29d8e89828be84e773f6809f4d13e0844dd7e7e4
EmmaluFox <emmalu.fox@outlook.com>
2020-01-24 16:08:29 +0000

    Multiplication

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the calculator!");
+/*
+            int x;
+            int y;
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine();
+*/
+
+            Console.WriteLine("Enter a number:");
+            string x = Console.ReadLine();
+            int myInt = int.Parse(x);
+            
+            Console.WriteLine("Enter another number:");
+            string y = Console.ReadLine();
+            int myIntTwo = int.Parse(y);
+            
+            Console.Write(myInt +" multiplied by " +myIntTwo +" = " +(myInt*myIntTwo));
+
         }
     }
 }
