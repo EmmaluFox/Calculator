e91ff9d1b228e6dc8ba9fd34b6e7ce1ea5eb35ba
EmmaluFox <emmalu.fox@outlook.com>
2020-01-27 17:43:12 +0000

    Refactored

--- Program.cs
+++ Program.cs
@@ -7,46 +7,61 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            PrintWelcomeMessage();
+            PerformOneCalculation();
+            Console.ReadLine();
+        }
+
+        static void PrintWelcomeMessage()
+        {
             Console.WriteLine(@"Welcome to the calculator!
 ************************************");
+        }
+
+        static void PerformOneCalculation()
+        {
             Console.WriteLine("Please enter the operator:");
             string op = Console.ReadLine();
-            
+
             Console.WriteLine($@"How many numbers would you like to {op}?");
             string alpha = Console.ReadLine();
             int numN = int.Parse(alpha);
-            
+
             Console.WriteLine("Enter the first number:");
             string gamma = Console.ReadLine();
             int initialNum = int.Parse(gamma);
-          
+
             string message = @"The answer is: ";
             int answer = initialNum;
-            
+
             for (int i = 1; i < numN; i++)
             {
                 Console.WriteLine("Enter a number:");
                 string beta = Console.ReadLine();
                 int newNum = int.Parse(beta);
-                
+
                 if (op == "+")
                 {
                     answer = answer + newNum;
-                } else if (op == "-")
+                }
+                else if (op == "-")
                 {
                     answer = answer - newNum;
-                } else if (op == "/") {
+                }
+                else if (op == "/")
+                {
                     answer = answer / newNum;
-                } else if (op == "*") {
+                }
+                else if (op == "*")
+                {
                     answer = answer * newNum;
                 }
             }
+
             Console.Write($@"{message}{answer}");
-            Console.ReadLine();
-            }
-            
-       
-            
-            
+
+        }
+
+
     }
 }
